dfeb7fe7a31cc71517500326b3629b87eec7b4c2
TonySiberian <Fynjy123456789065@gmail.com>
2023-02-12 21:42:31 +0300

    Освоены типы переменных

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,12 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		String somebody = "world";
+		System.out.println("Hello, " + somebody + "!");
+
+		int l = 6;
+		int s = l * l;
+		System.out.println("S квадрата со стороной " + l + " = " + s);
 	}
 
 }
