72a8d9c9bf85ee2ddd0a89d2e64b43a59f81ed9d
TonySiberian <Fynjy123456789065@gmail.com>
2023-02-16 07:49:01 +0300

    Задание №2 (попытка 3)

--- sandbox/src/main/java/ru/stqa/pft/point/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/point/Distance.java
@@ -6,8 +6,7 @@ public class Distance {
 
         Point p1 = new Point(12, 4);
         Point p2 = new Point(18, 8);
-        Point legs = new Point((p1.x - p2.x), (p1.y - p2.y));
-        System.out.println("Расстояние между точками " + "p1(" + p1.x + ", " + p1.y + ")" + " и " + "p2(" + p2.x + ", " + p2.y + ")" + " = " + legs.distance());
+        System.out.println("Расстояние между точками " + "p1(" + p1.x + ", " + p1.y + ")" + " и " + "p2(" + p2.x + ", " + p2.y + ")" + " = " + p1.distance(p2));
     }
 
 }
