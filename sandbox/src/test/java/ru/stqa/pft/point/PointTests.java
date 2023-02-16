87b70f82de01bb5915e49954a28bc50fdb21cc8e
TonySiberian <Fynjy123456789065@gmail.com>
2023-02-16 07:51:52 +0300

    Задание №3 (попытка 3)

--- sandbox/src/test/java/ru/stqa/pft/point/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/point/PointTests.java
@@ -9,8 +9,7 @@ public class PointTests {
     public void testPoint() {
         Point p1 = new Point(3, 6);
         Point p2 = new Point(4, 9);
-        Point legs = new Point((p1.x - p2.x), (p1.y - p2.y));
-        Assert.assertEquals(legs.distance(), 3.1622776601683795);
+        Assert.assertEquals(p1.distance(p2), 3.1622776601683795);
     }
 
 }
