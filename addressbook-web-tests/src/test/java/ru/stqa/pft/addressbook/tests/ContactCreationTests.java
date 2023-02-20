e75353277682a194ee28c12d3fb46252395009f2
TonySiberian <Fynjy123456789065@gmail.com>
2023-02-21 03:10:58 +0700

    Делегирование + рефакторинг создания контактов

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactCreationTests extends TestBase {
+
+    @Test
+    public void testContactCreation() throws Exception {
+        app.initContactCreation();
+        app.fillContactForm(new ContactData("test_first_name", "test_last_name", "test_address", "11111111111", "test_e-mail@gmail.com"));
+        app.submitContactCreation();
+        app.returnHomePage();
+    }
+}
