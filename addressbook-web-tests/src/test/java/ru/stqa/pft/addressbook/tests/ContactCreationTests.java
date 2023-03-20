02b404d69392d372f799f95986b28536612d2fea
TonySiberian <Fynjy123456789065@gmail.com>
2023-03-21 03:07:06 +0700

    Лекция 6.1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
+import java.io.File;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -17,11 +18,12 @@ public class ContactCreationTests extends TestBase {
         if (app.group().all().size() == 0) {
             app.group().create(new GroupData().withName("test1"));
         }
+        File photo = new File("src/test/resources/cat.jpg");
         ContactData contact = new ContactData()
                 .withFirstName("test_first_name").withLastName("test_last_name").withAddress("test_address0\ntest_address1\ntest_address2")
                 .withHomePhone("11111111111").withMobilePhone("22222222222").withWorkPhone("33333333333")
                 .withHomePhone2("44444444444").withEmail("test_e-mail@gmail.com").withEmail2("test_e-mail2@gmail.com")
-                .withEmail3("test_e-mail3@gmail.com").withGroup(app.group().gettingGroupName());
+                .withEmail3("test_e-mail3@gmail.com").withPhoto(photo).withGroup(app.group().gettingGroupName());
         app.contact().create(contact);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after  = app.contact().all();
@@ -37,11 +39,12 @@ public class ContactCreationTests extends TestBase {
         if (app.group().all().size() == 0) {
             app.group().create(new GroupData().withName("test1"));
         }
+        File photo = new File("src/test/resources/cat.jpg");
         ContactData contact = new ContactData()
                 .withFirstName("test_first_name'").withLastName("test_last_name").withAddress("test_address0\ntest_address1\ntest_address2")
                 .withHomePhone("11111111111").withMobilePhone("22222222222").withWorkPhone("33333333333")
                 .withHomePhone2("44444444444").withEmail("test_e-mail@gmail.com").withEmail2("test_e-mail2@gmail.com")
-                .withEmail3("test_e-mail3@gmail.com").withGroup(app.group().gettingGroupName());
+                .withEmail3("test_e-mail3@gmail.com").withPhoto(photo).withGroup(app.group().gettingGroupName());
         app.contact().create(contact);
         assertThat(app.contact().count(), equalTo(before.size()));
         Contacts after  = app.contact().all();
