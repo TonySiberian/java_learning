a765aa2cd656d633d2277f70bffade4c1166d475
TonySiberian <Fynjy123456789065@gmail.com>
2023-03-24 04:00:46 +0700

    Лекция 6.7, Задание №12, Задание №13

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
@@ -19,7 +21,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts() throws IOException {
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.xml"));
         String xml = "";
         String line = reader.readLine();
@@ -31,6 +33,20 @@ public class ContactCreationTests extends TestBase {
         xstream.processAnnotations(ContactData.class);
         xstream.allowTypes(new Class[]{ContactData.class});
         List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.json"));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
         return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
 
@@ -53,7 +69,7 @@ public class ContactCreationTests extends TestBase {
         return list.iterator();
     }
 
-    @Test(dataProvider = "validContacts")
+    @Test(dataProvider = "validContactsFromJson")
     public void testContactCreation(ContactData contact) throws Exception {
         app.goTo().homePage();
         Contacts before  = app.contact().all();
