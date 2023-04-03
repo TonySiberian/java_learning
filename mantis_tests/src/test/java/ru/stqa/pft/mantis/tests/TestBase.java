2b11f605e4e29d605394d32449360ebc2985b663
TonySiberian <Fynjy123456789065@gmail.com>
2023-04-04 03:22:15 +0700

    Задание 17

---
+++ mantis_tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.Browser;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+public class TestBase {
+
+    public static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", Browser.EDGE.browserName()));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+}
