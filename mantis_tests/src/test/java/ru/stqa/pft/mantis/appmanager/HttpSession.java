750fd3f8918d10b315ef595b0eb664bd1f160eef
TonySiberian <Fynjy123456789065@gmail.com>
2023-04-07 17:02:07 +0700

    Лекция  8.6 (исправленное)

--- mantis_tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis_tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -10,7 +10,6 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import java.io.Closeable;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
