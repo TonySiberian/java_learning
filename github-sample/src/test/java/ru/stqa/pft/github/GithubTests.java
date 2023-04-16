f53d7c345bad4474e785c6ec1ef524adae3a1daf
TonySiberian <Fynjy123456789065@gmail.com>
2023-04-16 22:35:12 +0700

    Лекция 9.6

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("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");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("TonySiberian", "java_learning")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
